67d691714bd2760f2bddddd8630e59d9a3b9379c
Vincent Delacourt <vincent.delacourt@joinhorizons.com>
2022-07-23 14:47:52 +0800

    Initial commit

---
+++ test/template-landing-page-ui-library-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TemplateLandingPageUiLibraryInfra from '../lib/template-landing-page-ui-library-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/template-landing-page-ui-library-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TemplateLandingPageUiLibraryInfra.TemplateLandingPageUiLibraryInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
